1d2c684f8d2ea3648dec1660cfdfacf8150a08b8
NikitaKlim <nikitaklimukwot@gmail.com>
2022-12-27 15:58:49 +0100

    0.5 createAntityAdapter

--- src/components/heroesFilters/filtersSlice.js
+++ src/components/heroesFilters/filtersSlice.js
@@ -1,11 +1,18 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
 import { useHttp } from '../../hooks/http.hook';
 
-const initialState = {
-    filters: [],
+const filtersAdapter = createEntityAdapter();
+
+const initialState = filtersAdapter.getInitialState({
     filtersLoadingStatus: 'idle',
     activeFilter: 'all'
-}
+});
+
+// const initialState = {
+//     filters: [],
+//     filtersLoadingStatus: 'idle',
+//     activeFilter: 'all'
+// }
 
 export const fetchFilters = createAsyncThunk(
     'heroes/fetchFilters',
@@ -26,7 +33,7 @@ const filtersSlice = createSlice({
             .addCase(fetchFilters.pending, state => {state.filtersLoadingStatus = 'loading'})
             .addCase(fetchFilters.fulfilled, (state, action) => {
                 state.filtersLoadingStatus = 'idle';
-                state.filters = action.payload;
+                filtersAdapter.setAll(state, action.payload);
             })
             .addCase(fetchFilters.rejected, state => {state.filtersLoadingStatus = 'error'})
             .addDefaultCase(() => {})
@@ -37,7 +44,11 @@ const filtersSlice = createSlice({
 const {actions, reducer} = filtersSlice;
 
 export default reducer;
+
+export const {selectAll} = filtersAdapter.getSelectors(state => state.filters);
+
 export const {filtersFetching,
-             filtersFetched,
-             filtersFetchingError,
-             filtersChanged} = actions;
+    filtersFetched,
+    filtersFetchingError,
+    filtersChanged
+} = actions;
